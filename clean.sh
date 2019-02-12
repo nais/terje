e8c954dc782d0abd1a9d949f1d63c974ac29041d
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2019-02-12 13:03:48 +0100

    Updated terje cleanup script.

--- clean.sh
+++ clean.sh
@@ -16,20 +16,10 @@ k delete clusterrole nais:team
 k delete clusterrole terje
 k delete clusterrolebinding terje
 k delete clusterrolebinding view:terje
-
-crb=`kubectl get clusterrolebinding -l "managed-by=terje" -o jsonpath="{.items[*].metadata.name}"`
-for b in $crb; do
-    k delete clusterrolebinding $b
-done
+k delete clusterrolebinding -l "managed-by=terje"
 
 namespaces=`kubectl get namespace -l "terje=enabled" -o jsonpath="{.items[*].metadata.name}"`
 for ns in $namespaces; do
-    roles=`kubectl get role --namespace $ns -l "managed-by=terje" -o jsonpath="{.items[*].metadata.name}"`
-    rolebindings=`kubectl get rolebinding --namespace $ns -l "managed-by=terje" -o jsonpath="{.items[*].metadata.name}"`
-    for r in $roles; do
-        k delete --namespace $ns role $r
-    done
-    for r in $rolebindings; do
-        k delete --namespace $ns rolebinding $r
-    done
+    k delete role --namespace $ns -l "managed-by=terje"
+    k delete rolebinding --namespace $ns -l "managed-by=terje"
 done
